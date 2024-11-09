351724e9fa948a7383e2ba32cdcaf565c12a5754
Evgeniya Kostyrya <zhenya241191@gmail.com>
2024-11-09 08:00:58 +0300

    modals

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,3 +3,6 @@ export { default as PaperClipIcon } from './paper-clip.hbs?raw';
 export { default as ArrowIcon } from './arrow.hbs?raw';
 export { default as CheckmarkIcon } from './checkmark.hbs?raw';
 export { default as ImageIcon } from './image.hbs?raw';
+export { default as PlusIcon } from './plus.hbs?raw';
+export { default as FileIcon } from './file.hbs?raw';
+export { default as LocationIcon } from './location.hbs?raw';
---
+++ src/components/modals/index.ts
@@ -0,0 +1,5 @@
+import './modals.pcss';
+export { default as AddDeleteUserModal } from './add-delete-user-modal.hbs?raw';
+export { default as FilesModal } from './files-modal.hbs?raw';
+export { default as AddDeleteUserSelectedModal} from './add-delete-user-selected-modal.hbs?raw';
+export { default as FileLoadModal} from './file-load-modal.hbs?raw';
