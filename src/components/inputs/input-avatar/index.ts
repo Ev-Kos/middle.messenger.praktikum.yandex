e93c9604e48949bc3932bb384767987ffc7226a2
Evgeniya Kostyrya <zhenya241191@gmail.com>
2024-11-09 09:09:25 +0300

    modals

--- src/components/inputs/input-avatar/index.ts
+++
@@ -1,2 +0,0 @@
-import './input-avatar.pcss';
-export { default as InputAvatar } from './input-avatar.hbs?raw';
---
+++ src/components/inputs/input-file/index.ts
@@ -0,0 +1,2 @@
+import './input-file.pcss';
+export { default as InputFile } from './input-file.hbs?raw';
