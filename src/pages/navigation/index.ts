49195d4a4bc2fac86aee22efca1dfbd7739bf7c2
Evgeniya Kostyrya <zhenya241191@gmail.com>
2024-11-22 12:48:26 +0300

    navigation

--- src/pages/navigation/index.ts
+++ src/pages/navigation/index.ts
@@ -1,2 +1,2 @@
 import './navigation.pcss';
-export { default as NavigationPage } from './navigation.hbs?raw';
+export { default as NavigationPage } from './navigation';
---
+++ src/pages/navigation/navigation.ts
@@ -0,0 +1,23 @@
+import Block from "../../core/block";
+
+export default class NavigationPage extends Block {
+  constructor() {
+    super('section', {
+      className: 'page',
+    })
+  }
+  public render(): string {
+    return `
+      <nav class="nav">
+        <ul class="nav__list">
+          <li><a href="#" class="nav__link" page="login">Авторизация</a></li>
+          <li><a href="#" class="nav__link" page="registration">Регистрация</a></li>
+          <li><a href="#" class="nav__link" page="chat">Чат</a></li>
+          <li><a href="#" class="nav__link" page="profile">Профиль</a></li>
+          <li><a href="#" class="nav__link" page="500">500</a></li>
+          <li><a href="#" class="nav__link" page="400">400</a></li>
+        </ul>
+      </nav>
+    `
+  }
+}
