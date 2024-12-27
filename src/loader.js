7725ca50c04c6dd2fe00a38e267ef9e42f7a19d5
Evgeniya Kostyrya <zhenya241191@gmail.com>
2024-12-27 08:20:53 +0300

    install

---
+++ src/loader.js
@@ -0,0 +1,47 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+
+export async function resolve(specifier, context, next) {
+    const nextResult = await next(specifier, context);
+
+    if (specifier.endsWith('.hbs?raw')
+        || specifier.endsWith('.pcss') || specifier.endsWith('.hbs')) {
+        return {
+            format: specifier.endsWith('.hbs?raw')
+              ? 'hbs?raw'
+              : specifier.endsWith('.pcss')
+              ? 'pcss'
+              : 'hbs',
+            shortCircuit: true,
+            url: nextResult.url,
+        };
+    }
+
+    return nextResult;
+}
+
+export async function load(url, context, next) {
+    const extension = url.split('.').pop();
+
+    if (context.format === 'hbs?raw'
+      || context.format === 'pcss' || context.format === 'hbs') {
+        const rawSource = '' + await fs.readFile(fileURLToPath(url));
+
+        if (extension === 'pcss') {
+            return {
+                format: 'module',
+                shortCircuit: true,
+                source: '',
+            };
+        }
+        if (extension === 'hbs?raw') {
+            return {
+                format: 'module',
+                shortCircuit: true,
+                source:  `export default "${rawSource}"`,
+            };
+        }
+    }
+
+    return next(url, context);
+}
